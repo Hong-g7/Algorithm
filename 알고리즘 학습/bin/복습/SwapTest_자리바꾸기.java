adefd0220af9801b655c73dd3bff49cee21507e5
Gwon Hong <rnjsghd97@naver.com>
2023-11-30 14:32:17 +0900

    no message

---
+++ 알고리즘 학습/bin/복습/SwapTest_자리바꾸기.java
@@ -0,0 +1,21 @@
+package 복습;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SwapTest_자리바꾸기 {
+
+	public static void main(String[] args) {
+		Solution2 s = new Solution2();
+		String s2 = "abcabcadc";
+		String result = s.solution(s2);
+		System.out.println(result);
+	}
+}
+
+class Solution2 {
+	public String solution(String s2) {
+		String answer = "";
+		return answer;
+	}
+}
---
+++ 알고리즘 학습/bin/복습/버블정렬.java
@@ -0,0 +1,21 @@
+package 복습;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 버블정렬 {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		String s2 = "abcabcadc";
+		String result = s.solution(s2);
+		System.out.println(result);
+	}
+}
+
+class Solution {
+	public String solution(String s2) {
+		String answer = "";
+		return answer;
+	}
+}
